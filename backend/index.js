a303051e72fc93c7048e65b577490ff4e99c8da7
parthin-12 <201901113@daiict.ac.in>
2022-07-14 18:46:48 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import database from "./Config/database.js";
 import cloudinary from  "cloudinary";
 import {Server} from "socket.io";
+import express from "express";
 import path from "path";
 import { fileURLToPath } from 'url';
 
